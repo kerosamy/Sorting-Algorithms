33595a2c8c840578971089f91a7d5304a6d2919b
caroline-2 <carolinehanna510@gmail.com>
2025-03-14 02:32:52 +0200

    Added time in the Testing

--- src/test/java/SorterTest.java
+++ src/test/java/SorterTest.java
@@ -29,13 +29,31 @@ public class SorterTest {
             ArrayList<Integer> expected = new ArrayList<>(arr);
             Collections.sort(expected);
 
+            long startNLogN = System.nanoTime();
             ArrayList<Integer> resultNLogN = sortNLogN.sort(new ArrayList<>(arr), false);
+            long endNLogN = System.nanoTime();
+            long timeNLogN = endNLogN - startNLogN;
+
+            long startN2 = System.nanoTime();
             ArrayList<Integer> resultN2 = sortN2.sort(new ArrayList<>(arr), false);
+            long endN2 = System.nanoTime();
+            long timeN2 = endN2 - startN2;
+
+            long startN = System.nanoTime();
             ArrayList<Integer> resultN = sortN.sort(new ArrayList<>(arr), false);
+            long endN = System.nanoTime();
+            long timeN = endN - startN;
 
             assertEquals(expected, resultNLogN, "O(N log N) sort failed on test " + (i + 1));
             assertEquals(expected, resultN2, "O(N²) sort failed on test " + (i + 1));
             assertEquals(expected, resultN, "O(N) sort failed on test " + (i + 1));
+
+            System.out.println("Test " + (i + 1) + " (Size: " + size + ")");
+            System.out.println("O(N²) Sort Time: " + timeN2 / 1_000_000.0 + " ms");
+            System.out.println("O(N log N) Sort Time: " + timeNLogN / 1_000_000.0 + " ms");
+            System.out.println("O(N) Sort Time: " + timeN / 1_000_000.0 + " ms");
+            System.out.println("-------------------------------");
+
         }
     }
 }
